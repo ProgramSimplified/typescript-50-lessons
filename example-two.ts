0cdb8245d45eaf56c6a2088347e18782fab7dc3c
杨勇 <1994101600@qq.com>
2021-04-30 11:38:22 +0800

    Structural Typing and Excess Property Checks

--- example-two.ts
+++ example-two.ts
@@ -1,5 +1,37 @@
+type Article = {
+  title: string,
+  price: number,
+  vat: number,
+  stock: number,
+  description: string
+}
+
 function addVAT(price: number, vat = 0.2): number {
   return price * (1 + vat)
 }
 
-const vatPriceWrong = addVAT(100, 0.2)
+let deliveryAddresses: string[] = [
+  '421 Smashing Hill, 90210',
+  '221b Paw-ker Street',
+  '4347 Whiskers-ia Lane'
+]
+
+function selectDeliveryAddress(addressOrIndex: unknown) {
+  if (
+    typeof addressOrIndex === 'number' &&
+    addressOrIndex < deliveryAddresses.length
+  ) {
+    return deliveryAddresses[addressOrIndex]
+  } else if (typeof addressOrIndex === 'string') {
+    return addressOrIndex
+  }
+  return ''
+}
+
+const book = {
+  title: 'Form Design Patterns by Adam Silver',
+  price: 32.77,
+  vat: 0.19,
+  stock: 1000,
+  description: 'A practical book on accessibility and forms'
+}
