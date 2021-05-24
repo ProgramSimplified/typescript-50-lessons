e8637b19eb4670ea6b9064795251381977649201
杨勇 <1994101600@qq.com>
2021-05-24 19:12:22 +0800

    callbacks

--- function.ts
+++ function.ts
@@ -13,3 +13,51 @@ function search(query: string, tags?: string[]): Promise<Result[]> {
 
   return fetch(`/search${queryString}`).then((response) => response.json())
 }
+
+type SearchFn = typeof search
+
+type Query = {
+  query: string
+  tags?: string[]
+  assemble: Assemble
+}
+
+type Assemble = (includeTags: boolean) => string
+
+const query: Query = {
+  query: 'Ember',
+  tags: ['javascript'],
+  assemble(includeTags = false) {
+    let query = `?query=${this.query}`
+    if (includeTags && typeof this.tags !== 'undefined') {
+      query += `&${this.tags.join(',')}`
+    }
+    return query
+  }
+}
+
+declare function displaySearch(
+  inputId: string,
+  outputId: string,
+  search: SearchFn
+): void
+
+displaySearch('searchField', 'result', function (query, tags) {
+  return Promise.resolve([
+    {
+      title: `The ${query} test book`,
+      url: `/${query}-design-patterns`,
+      abstract: `A practical book on ${query}`
+    }
+  ])
+})
+
+const testSearch: SearchFn = function (term) {
+  return Promise.resolve([
+    {
+      title: `The ${term} test book`,
+      url: `/${term}-design-patterns`,
+      abstract: `A practical book on ${term}`
+    }
+  ])
+}
