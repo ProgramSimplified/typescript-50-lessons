876011f4c5a09dff0c62df36ed2b455b1651046f
杨勇 <1994101600@qq.com>
2021-05-25 14:07:39 +0800

    chore: function

--- function.ts
+++ function.ts
@@ -4,60 +4,22 @@ type Result = {
   abstract: string
 }
 
-function search(query: string, tags?: string[]): Promise<Result[]> {
+function search(
+  query: string,
+  callback: (results: Result[]) => void,
+  tags?: string[]
+) {
   let queryString = `?query=${query}`
 
   if (tags && tags.length) {
     queryString += `&tags=${tags.join()}`
   }
 
-  return fetch(`/search${queryString}`).then((response) => response.json())
+  fetch(`/search${queryString}`)
+    .then((response) => response.json() as Promise<Result[]>)
+    .then((results) => void callback(results))
 }
 
-type SearchFn = typeof search
-
-type Query = {
-  query: string
-  tags?: string[]
-  assemble: Assemble
-}
-
-type Assemble = (includeTags: boolean) => string
-
-const query: Query = {
-  query: 'Ember',
-  tags: ['javascript'],
-  assemble(includeTags = false) {
-    let query = `?query=${this.query}`
-    if (includeTags && typeof this.tags !== 'undefined') {
-      query += `&${this.tags.join(',')}`
-    }
-    return query
-  }
-}
-
-declare function displaySearch(
-  inputId: string,
-  outputId: string,
-  search: SearchFn
-): void
-
-displaySearch('searchField', 'result', function (query, tags) {
-  return Promise.resolve([
-    {
-      title: `The ${query} test book`,
-      url: `/${query}-design-patterns`,
-      abstract: `A practical book on ${query}`
-    }
-  ])
+search('Ember', function (results) {
+  console.log(results)
 })
-
-const testSearch: SearchFn = function (term) {
-  return Promise.resolve([
-    {
-      title: `The ${term} test book`,
-      url: `/${term}-design-patterns`,
-      abstract: `A practical book on ${term}`
-    }
-  ])
-}
