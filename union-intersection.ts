13df3f118e08b409c7918e5477dc6adeffcdd6bf
杨勇 <1994101600@qq.com>
2021-06-01 20:24:10 +0800

    union and intersection type

---
+++ union-intersection.ts
@@ -0,0 +1,33 @@
+type Talk = {
+  abstract: string
+  speaker: string
+}
+
+type TechEventBase = {
+  title: string
+  description: string
+  date: Date
+  capacity: number
+  rsvp: number
+  kind: string
+}
+
+type Conference = TechEventBase & {
+  location: string
+  price: number
+  talks: Talk[]
+}
+
+type Meetup = TechEventBase & {
+  location: string
+  price: string
+  talks: Talk[]
+}
+
+type Webinar = TechEventBase & {
+  url: string
+  price?: number
+  talks: Talk
+}
+
+type TechEvent = Webinar | Conference | Meetup
