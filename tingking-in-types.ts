eb67895c49387c64e49c30b05d11759b85dbbf5b
杨勇 <1994101600@qq.com>
2021-07-14 20:50:52 +0800

    Lesson 43: Promisify

--- tingking-in-types.ts
+++ tingking-in-types.ts
@@ -1,9 +1,45 @@
 declare function loadFile(filename: string, cb: (result: string) => void)
 
-const loadFilePromise = promisefy(loadFile)
+const loadFilePromise = promisify(loadFile)
 
 loadFilePromise('./package.json').then((result) => result.toUpperCase())
 
-declare function promisefy<Fun extends FunctionWithCallback>(
+type FunctionWithCallback<T extends any[]> = (
+  ...t: [...T, (...args: any) => any]
+) => any
+
+type InferArguments<T> = T extends (
+  ...t: [...infer R, (...args: any) => any]
+) => any
+  ? R
+  : never
+
+type InferResults<T> = T extends (
+  ...t: [...infer K, (res: infer R) => any]
+) => any
+  ? R
+  : never
+
+type PromisifiedFunction<T> = (
+  ...args: InferArguments<T>
+) => Promise<InferResults<T>>
+
+function promisify<Fun extends FunctionWithCallback>(
   fun: Fun
-): PromisefiedFunction<Fun>
+): PromisifiedFunction<Fun> {
+  return function (...args: InferArguments<Fun>) {
+    return new Promise((resolve) => {
+      fun.call(null, ...args, (result: InferResults<Fun>) => {
+        resolve(result)
+      })
+    })
+  }
+}
+
+function addAsync(x: number, y: number, cb: (result: number) => void) {
+  cb(x + y)
+}
+
+const addProm = promisify(addAsync)
+// x is number!
+addProm(1, 2).then((x) => x)
