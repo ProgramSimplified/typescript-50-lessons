c706f6aa13c3ef38cc137b12849156b55d5176c5
杨勇 <1994101600@qq.com>
2021-07-19 16:54:48 +0800

    Lesson 44: JSONify

--- tingking-in-types.ts
+++ tingking-in-types.ts
@@ -43,3 +43,66 @@ function addAsync(x: number, y: number, cb: (result: number) => void) {
 const addProm = promisify(addAsync)
 // x is number!
 addProm(1, 2).then((x) => x)
+
+type Widget = {
+  toJSON(): {
+    kind: 'Widget'
+    data: Date
+  }
+}
+
+type Item = {
+  // Regular primitive types
+  text: string
+  count: number
+  // Options get preserved
+  choice: 'yes' | 'no' | null
+  // Functions get dropped.
+  func: () => void
+  // Nested elements need to be parsed
+  // as well
+  nested: {
+    isSaved: boolean
+    data: [1, undefined, 2]
+  }
+  // A pointer to another type
+  widget: Widget
+  // The same object referenced again
+  children?: Item[]
+}
+
+type JSONified<T> = JSONifiedValue<T extends { toJSON(): infer U } ? U : T>
+
+type JSONifiedValue<T> = T extends string | number | boolean | null
+  ? T
+  : T extends Function
+  ? never
+  : T extends object
+  ? JSONifiedObject<T>
+  : T extends Array<infer U>
+  ? JSONifiedArray<U>
+  : never
+
+type JSONifiedObject<T> = {
+  [P in keyof T]: JSONified<T[P]>
+}
+
+type UndefinedAsNull<T> = T extends undefined ? null : T
+
+type JSONifiedArray<T> = Array<UndefinedAsNull<JSONified<T>>>
+
+class Serializer<T> {
+  serialize(inp: T): string {
+    return JSON.stringify(inp)
+  }
+
+  deserialize(inp: string): JSONified<T> {
+    return JSON.parse(inp)
+  }
+}
+
+const itemSerializer = new Serializer<Item>()
+
+const serialization = itemSerializer.serialize(anItem)
+
+const obj = itemSerializer.deserializer(serialization)
